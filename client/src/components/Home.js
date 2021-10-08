39ded90783f7bffadc1c200ffeb6868de2443b82
Ankit <ankitrastogi31200@gmail.com>
2021-10-08 21:52:01 +0530

    home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import VideoCard from "./VideoCard";
+import '../style/Home.css'
 
 export default function Home() {
     return (
-        <div>
-            This is Home
+        <div className='home'>  
+            {
+                Array.from({length: 5*Math.floor((Math.random() * 6) + 3)}, (x, i) => <VideoCard number={i}/>)
+            }  
         </div>
     )
 }
