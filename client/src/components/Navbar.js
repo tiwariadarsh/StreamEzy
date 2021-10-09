b97e6602dc1d3021d6e4ad98de0f2b05f071541f
Ankit <ankitrastogi31200@gmail.com>
2021-10-09 11:52:57 +0530

    changes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Button, Navbar, Nav, Container } from "react-bootstrap";
 
 export default function Navbarx () {
   return (
-    <Navbar bg="dark" variant="dark" style={{ position: "absolute", left:"0",right:"0" }}>
+    <Navbar bg="dark" variant="dark" style={{ width:"100vw"}}>
       <Container>
         <Navbar.Brand href="/">
           <h4>StreamEzy</h4>
