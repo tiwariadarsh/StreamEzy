e74107b68cc3e3b61fdffd7b24746ef20c87a808
unknown <ratfox124@gmail.com>
2021-10-10 01:05:43 +0530

    Go live

---
+++ client/src/components/viewpage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+ 
+import ReactPlayer from 'react-player';
+const streamurl="https://cdn.livepeer.com/hls/f2c08zvz4pkh3xa0/index.m3u8"
+
+
+
+
+
+
+export default function Viewpage() {
+    return (
+      
+      <div className="App">
+        
+      <header className="App-header">
+        <ReactPlayer
+        url={streamurl}
+        width="100%"
+        heigh="100%"
+        />
+     </header>
+
+   </div>
+    );
+}
+
+
