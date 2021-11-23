3cc5e1ee8c50c147568d1c380d5ff370e8907fd3
tiwariadarsh <adarshtiwari532000@gmail.com>
2021-11-23 19:01:50 +0530

    Userprofile page fronend change

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,100 +1,198 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import "../style/UserProfile.css";
-import { doc, setDoc, getDoc } from "firebase/firestore"; 
-import {db} from '../firebase'
+import { doc, setDoc, getDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 export default function UserProfile() {
+  const [address, setaddress] = useState("address");
+  const [name, setname] = useState("");
+  const [dob, setdob] = useState("");
+  const [bio, setbio] = useState("");
 
-  const [address, setaddress] = useState('address')
-  const [name, setname] = useState('')
-  const [dob, setdob] = useState('')
-  const [bio, setbio] = useState('')
-
-  const [displayname, setdisplayname] = useState('')
-  const [displaybio, setdisplaybio] = useState('')
-  const [displaydob, setdisplaydob] = useState('')
-  const [displaysubscribe, setdisplaysubscribe] = useState(0)
-
-  const [edit, setedit] = useState(false)
+  const [displayname, setdisplayname] = useState("Loading..");
+  const [displaybio, setdisplaybio] = useState("Loading..");
+  const [displaydob, setdisplaydob] = useState("Loading..");
+  const [displaysubscribe, setdisplaysubscribe] = useState(0);
 
   const loadUserDetails = async () => {
     try {
       const docRef = doc(db, "users", address);
       const docSnap = await getDoc(docRef);
       console.log(docSnap.data());
-      setdisplayname(docSnap.data().name)
-      setdisplaysubscribe(docSnap.data().subscribers?.length)
-      setdisplaybio(docSnap.data().bio)
-      setdisplaydob(docSnap.data().dob)
-      window.localStorage.setItem('currentuser',JSON.stringify(docSnap.data()))
+      setdisplayname(docSnap.data().name);
+      setdisplaysubscribe(docSnap.data().subscribers?.length);
+      setdisplaybio(docSnap.data().bio);
+      setdisplaydob(docSnap.data().dob);
+      window.localStorage.setItem(
+        "currentuser",
+        JSON.stringify(docSnap.data())
+      );
     } catch (error) {
-      alert(error.message)
+      alert(error.message);
     }
-  }
+  };
 
   useEffect(() => {
     loadUserDetails();
-  }, [])
+  }, []);
 
-  const updateDetails = async () =>{
+  const updateDetails = async () => {
     try {
       const docRef = await setDoc(doc(db, "users", address), {
-        name:name,
-        dob:dob,
-        bio:bio,
-        comments:[],
-        likedVideos:[],
-        subscribers:[],
-        address:address
+        name: name,
+        dob: dob,
+        bio: bio,
+        comments: [],
+        likedVideos: [],
+        subscribers: [],
+        address: address,
       });
-      setdob('')
-      setname('')
-      setbio('')
+      setdob("");
+      setname("");
+      setbio("");
       loadUserDetails();
     } catch (error) {
-      alert(error.message)
+      alert(error.message);
     }
-  }
+  };
 
   return (
-    <>
-      <div className="container" style={{backgroundColor:"black", marginTop: "8%", width:"70%"}}>
-        <div className="container" style={{width:"99%"}}>
-          <div className="row sh" style={{backgroundColor:"white"}}>
-            
-            <div className="col-lg-4">
-              <div className="card-img1" style={{height:"17rem"}} ></div>
+    <div
+      className="col-xl-6 col-md-12"
+      style={{
+        marginLeft: "27rem",
+        marginTop: "11rem",
+        boxSizing: "border-box",
+      }}
+    >
+      <div className="card user-card-full">
+        <div className="row m-l-0 m-r-0">
+          <div className="col-sm-4 bg-c-lite-green user-profile">
+            <div className="card-block text-center text-white">
+              <div className="m-b-25">
+                {" "}
+                <img
+                  src="https://img.icons8.com/bubbles/150/000000/user.png"
+                  className="img-radius"
+                  alt="User-Profile-Image"
+                />{" "}
+              </div>
+              <h6 className="f-w-600">{displayname} </h6>
+              <Popup
+                trigger={<i className="far fa-edit m-t-10 f-16"></i>}
+                // modal
+                position="right center"
+              >
+                <form className="popup-form">
+                  <h5>
+                    {" "}
+                    <b> Edit your details: </b>{" "}
+                  </h5>
+                  <br />
+                  <input
+                    name="name"
+                    type="text"
+                    class="feedback-input"
+                    onChange={(e) => setname(e.target.value)}
+                    placeholder="Name"
+                  />
+                  <textarea
+                    name="email"
+                    type="text"
+                    class="feedback-input"
+                    onChange={(e) => setbio(e.target.value)}
+                    placeholder="Bio"
+                  />
+                  <input
+                    name="dob"
+                    class="feedback-input"
+                    type="date"
+                    onChange={(e) => setdob(e.target.value)}
+                    placeholder="Date of Birth"
+                  />
+                  <input type="submit" onClick={updateDetails} value="SUBMIT" />
+                </form>
+              </Popup>
             </div>
-              
-            <div className = "col-lg-2">
-              <p><i className="fas fa-user" style={{color:"violet"}}></i> {displayname}</p>
-            </div>
-      
-            <div className="col-lg-2">
-              <p><i className="fas fa-calendar-week" style={{color:"orange"}}></i>{displaydob}</p>
-            </div>  
-      
-            <div className="col-lg-2">
-              <p><i className="fas fa-location-arrow" style={{color:"blue"}}></i>{displaybio}</p>
-            </div>
-
-            <br/>
-            <div className="col-lg-2" style={{marginLeft:"23rem", marginTop : "-14rem"}}>
-              <p><i className="fas fa-users" style={{color:"gold"}}></i> Subscribers: {displaysubscribe}</p>
-            </div>
-            <br/>
-            <div className="col-lg-2" style={{marginLeft:"21rem", marginTop : "-12rem"}}>
-              <p><i className="fas fa-video" style={{color:"red"}}></i> Videos</p>
+          </div>
+          <div className="col-sm-8">
+            <div className="card-block">
+              <h6 className="m-b-20 p-b-5 b-b-default f-w-600">Information</h6>
+              <div className="row">
+                <div className="col-sm-6">
+                  <p className="m-b-10 f-w-600">Bio</p>
+                  <h6 className="text-muted f-w-400">{displaybio} </h6>
+                </div>
+                <div className="col-sm-6">
+                  <p className="m-b-10 f-w-600">D.O.B</p>
+                  <h6 className="text-muted f-w-400">{displaydob} </h6>
+                </div>
+                <div className="col-sm-6">
+                  <p className="m-b-10 f-w-600">Location</p>
+                  <h6 className="text-muted f-w-400">India</h6>
+                </div>
+                <div className="col-sm-6">
+                  <p className="m-b-10 f-w-600">Email</p>
+                  <h6 className="text-muted f-w-400">youremail@gmail.com</h6>
+                </div>
+              </div>
+              <h6 className="m-b-20 m-t-40 p-b-5 b-b-default f-w-600">
+                Activity on StreamEzy
+              </h6>
+              <div className="row">
+                <div className="col-sm-6">
+                  <p className="m-b-10 f-w-600">Subscribers</p>
+                  <h6 className="text-muted f-w-400">{displaysubscribe}</h6>
+                </div>
+                <div className="col-sm-6">
+                  <p className="m-b-10 f-w-600">Videos</p>
+                  <h6 className="text-muted f-w-400">will upload soon</h6>
+                </div>
+              </div>
+              <ul className="social-link list-unstyled m-t-40 m-b-10">
+                <li>
+                  <a
+                    href="#!"
+                    data-toggle="tooltip"
+                    data-placement="bottom"
+                    title=""
+                    data-original-title="facebook"
+                    data-abc="true"
+                  >
+                    <i className="fab fa-facebook-f"></i>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    data-toggle="tooltip"
+                    data-placement="bottom"
+                    title=""
+                    data-original-title="twitter"
+                    data-abc="true"
+                  >
+                    <i class="fab fa-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    data-toggle="tooltip"
+                    data-placement="bottom"
+                    title=""
+                    data-original-title="instagram"
+                    data-abc="true"
+                  >
+                    <i class="fab fa-instagram"></i>
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
       </div>
-      <div className='formUserEdit'>
-        <button className='editUserProfile' onClick={updateDetails}>Edit</button>
-        <input value={name} onChange={(e)=>setname(e.target.value)} type="text" name='name' />
-        <input value={bio} onChange={(e)=>setbio(e.target.value)} type="text" name='bio' />
-        <input value={dob} onChange={(e)=>setdob(e.target.value)} type="date" name='dob' />
-      </div>
-    </>
-  )
+    </div>
+  );
 }
